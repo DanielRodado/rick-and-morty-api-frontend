bd9bc97ddf58c1d8c5ac88c24a50d57eeebf99c8
DanielRodado <d4nielrodado@gmail.com>
2024-05-29 16:46:02 -0500

    Se realizó la página de 'episode.html' en donde están detalles de los mismos

---
+++ js/episode.js
@@ -0,0 +1,62 @@
+"use strict";
+
+const { createApp } = Vue;
+
+createApp({
+    data() {
+        return {
+            idEpisode: null,
+            episode: [],
+            characters: [],
+            checkVisibilityCards: false,
+            loader: true,
+        };
+    },
+
+    created() {
+        this.getURLParams();
+        this.getEpisode();
+    },
+
+    methods: {
+        getEpisode() {
+            axios(`https://rickandmortyapi.com/api/episode/${this.idEpisode}`)
+                .then(({ data }) => {
+                    this.episode = data;
+                    for (const URLcharacter of data.characters) {
+                        this.getCharacters(URLcharacter);
+                    }
+                    setTimeout(() => {
+                        this.loader = false;
+                    }, 1500);
+                })
+                .catch((err) => console.log(err));
+        },
+        getCharacters(URLOfCharacter) {
+            axios(URLOfCharacter)
+                .then(({ data }) => {
+                    this.getNameOfEpisode(data.episode[0], data);
+                })
+                .catch((err) => console.log(err));
+        },
+        getNameOfEpisode(URLOfEpisode, character) {
+            axios(URLOfEpisode)
+                .then(({ data }) => {
+                    character.nameOfEpisode = data.name;
+                    this.characters.push(character);
+                })
+                .catch(() => (character.nameOfEpisode = "undefined"));
+        },
+        getURLParams() {
+            this.idEpisode = parseInt(
+                new URLSearchParams(document.location.search).get("episode")
+            );
+        },
+        windowsTop() {
+            scrollTo({
+                top: 0,
+                behavior: "smooth",
+            });
+        },
+    },
+}).mount("#app");
